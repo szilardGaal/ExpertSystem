14898d53c40caec3306bda728e9de50b658ea30c
Taraszovics Péter <gtarapeti@gmail.com>
2019-01-30 09:31:50 +0100

    esprovider alfa version

--- pcexpert/src/main/java/com/Codecool/ESProvider.java
+++ pcexpert/src/main/java/com/Codecool/ESProvider.java
@@ -1,5 +1,8 @@
 package com.Codecool;
 
+import java.util.List;
+import java.util.Set;
+
 public class ESProvider {
 
     FactRepository fRepo;
@@ -28,5 +31,29 @@ public class ESProvider {
     }
 
     public RuleRepository getRRepo() { return rRepo;}
+
+
+    public void matchAnswers(){
+
+    List<Question> qlist = rRepo.getQuestionsList();
+
+    for (int i = 0;i<qlist.size();i++ ){
+        qlist.get(i).getAnswer();
+        qlist.get(i).getId();
     }
+    List<Fact> flist = fRepo.getFactsList();
+    for (int i = 0; i< flist.size();i++){
+        Set<String> tempset = flist.get(i).getIdSet();
+        for (String id : tempset){
+            if (flist.get(i).getValueById(id) != qlist.get(i).getAnswer().getValue()) {
+
+                continue;
+
+            }
+            System.out.println("addtolist");
+        }
+    }
+
+    }
+}
 
