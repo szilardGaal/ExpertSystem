bdc1375b7b21df6f88f6bd17fcf0b9c54ab6cabc
Zoltan <zolee95@gmail.com>
2019-01-30 11:14:02 +0100

    formatted and working

--- pcexpert/src/main/java/com/Codecool/App.java
+++ pcexpert/src/main/java/com/Codecool/App.java
@@ -8,17 +8,6 @@ public class App {
         FactParser fParser = new FactParser("src/main/data/Facts.xml");
         RuleParser rParser = new RuleParser("src/main/data/Rules.xml");
         ESProvider provider = new ESProvider(fParser, rParser);
-        List<Fact> whatever = provider.getFRepo().getFactsList();
-        for (Fact fact : whatever) {
-            System.out.println(fact);
-            System.out.println(fact.getIdSet());
-        }
-        List<Rule> whatever2 = provider.getRRepo().getRulesList();
-        for (Rule rule : whatever2) {
-            System.out.println(rule);
-            System.out.println(rule.getIdSet());
-            System.out.println(rule.getAnswerSet());
-        }
 
         provider.getRRepo().getAnswers();
         System.out.println(provider.getRRepo().getQuestionsList());
